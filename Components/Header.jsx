a08dd4116aeecf471ba8ecfb736564c448700620
bayuhusada <nekobay08@gmail.com>
2024-12-31 00:43:35 +0800

    test

---
+++ Components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='py-5 px-5 md:px-12 lg:px-28'>
+  <div className='flex items-center justify-between'>
+    <h1 className='text-3xl font-bold text-blue-600'>BLOG</h1>
+  </div>
+</div>
+  )
+}
+
+export default Header
