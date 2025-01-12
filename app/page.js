19c939ebf3b4cf22c6bbd569a4d112f6e7724e39
bayuhusada <nekobay08@gmail.com>
2025-01-12 17:43:39 +0800

    tes

--- app/page.js
+++ app/page.js
@@ -2,6 +2,8 @@
 import BlogList from "@/Components/BlogList";
 import Footer from "@/Components/Footer";
 import Header from "@/Components/Header";
+import "./globals.css";
+
 
 export default function Home() {
   return (
